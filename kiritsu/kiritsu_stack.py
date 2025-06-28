0b7824c4fdad440b736bb47b7873c94405f9c051
Avneet Singh <singhab2024@gmail.com>
2025-06-27 22:13:12 -0700

    Added S3

--- kiritsu/kiritsu_stack.py
+++ kiritsu/kiritsu_stack.py
@@ -1,8 +1,5 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+from aws_cdk import Stack
+from aws_cdk import aws_s3 as s3
 from constructs import Construct
 
 class KiritsuStack(Stack):
@@ -10,10 +7,4 @@ class KiritsuStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "KiritsuQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        s3.Bucket(self, "MyFirstResouce", versioned=True)
